d691bfff366d5df8eabbccea490b59f9d7290af6
Kris Arnold <karnold@imamuseum.org>
2012-05-24 14:54:54 -0400

    added sql dump for creating table

---
+++ tracker.sql
@@ -0,0 +1,34 @@
+-- MySQL dump 10.13  Distrib 5.1.62, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: islandtracker
+-- ------------------------------------------------------
+-- Server version   5.1.62-0ubuntu0.10.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tracker`
+--
+
+DROP TABLE IF EXISTS `tracker`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tracker` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` int(11) DEFAULT NULL,
+  `lat` int(11) DEFAULT NULL,
+  `lng` int(11) DEFAULT NULL,
+  `speed` int(3) DEFAULT NULL,
+  `course` varchar(5) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
